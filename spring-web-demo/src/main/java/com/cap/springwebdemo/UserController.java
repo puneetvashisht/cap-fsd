26469df70d32142968292c2f8b846c9296ede12a
Puneet <puneetvashisht@gmail.com>
2020-12-16 11:16:01 +0530

    test

--- spring-web-demo/src/main/java/com/cap/springwebdemo/UserController.java
+++ spring-web-demo/src/main/java/com/cap/springwebdemo/UserController.java
@@ -2,12 +2,18 @@ package com.cap.springwebdemo;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,12 +25,6 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
-	@GetMapping("/user")
-	public User getUser() {
-		User user = new User("test@test", "testtest");
-		return user;	
-		
-	}
 	
 	@GetMapping("/users")
 	public List<User> getAllUser() {
@@ -32,17 +32,52 @@ public class UserController {
 		
 	}
 	
-	@GetMapping("/users/{id}")
-	public User getUserById(@PathVariable("id") int id) {
+//	@GetMapping("/users/{id}")
+	@RequestMapping(path = "/users/{id}", method = RequestMethod.GET)
+	public ResponseEntity<User> getUserById(@PathVariable("id") int id) throws Exception {
+		ResponseEntity<User>  re = null;
+		if(true)
+		throw new Exception("deliberate exception");
+		
 		System.out.println("Recieved id on path: "+ id);
 		// code here to fetch user by id
-		return null;
+		Optional<User> user = userRepo.findById(id);
+		if(user.isPresent()) {
+			User userFound = user.get();
+			re = new ResponseEntity<>(userFound, HttpStatus.OK);
+		}
+		else {
+			re = new ResponseEntity<>( HttpStatus.NOT_FOUND);
+		}
+		return re;
+		
+	}
+	
+	@GetMapping("/user")
+	public ResponseEntity<User> getUserByName(@RequestParam("name") String name) {
+		ResponseEntity<User>  re = null;
+		System.out.println(name);
+		User existingUser = userRepo.findByEmail(name);
+		if(existingUser == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(existingUser, HttpStatus.OK);
+
 		
 	}
 	
 	@PostMapping("/users")
-	public void addUser(@RequestBody User user) {
+	public ResponseEntity<String> addUser(@RequestBody User user) {
+		
+		User existingUser = userRepo.findByEmail(user.getEmail());
+		if(existingUser != null) {
+			return new ResponseEntity<String>("User already exists!!", HttpStatus.CONFLICT);
+		}
+		
 		userService.register(user);
+		
+		ResponseEntity<String> re = new ResponseEntity<>("successfully registered user!!", HttpStatus.CREATED);
+		return re;
 	}
 	
 	@GetMapping("/hello")
--- spring-web-demo/src/main/java/com/cap/springwebdemo/UserRepo.java
+++ spring-web-demo/src/main/java/com/cap/springwebdemo/UserRepo.java
@@ -3,5 +3,12 @@ package com.cap.springwebdemo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
+	
+//	"select u from User u where u.email:email"
+	
+	public User findByEmail(String email);
+	
+//	@Query
+//	b
 
 }
